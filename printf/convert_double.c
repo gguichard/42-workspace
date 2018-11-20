4d08eb5f20d7def95902d0eb38456f6b8a7c4c27
Gwennael Guichard <gguichar@student.42.fr>
2018-11-20 08:09:24 +0100

    WIP %d printf

--- printf/convert_double.c
+++ printf/convert_double.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 16:10:19 by gguichar          #+#    #+#             */
-/*   Updated: 2018/11/20 00:15:58 by gguichar         ###   ########.fr       */
+/*   Updated: 2018/11/20 08:08:58 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,16 @@
 
 void	convert_double(t_token *token, va_list ap, t_buf *buf)
 {
-	long double	value;
-	long long	pow;
-	int			i;
+	long double			value;
+	double				pow;
+	int					i;
 
 	if (token->modifiers & LUP_MODIFIER)
 		value = va_arg(ap, long double);
 	else
 		value = va_arg(ap, double);
-	buf->str = ft_lltoa((long long)value);
-	if (!(buf->str))
+	if (!(buf->str = ft_lltoa((long long)value)))
 		exit(1);
-	value = value < 0 ? -value : value;
 	if (token->precision < 0)
 		token->precision = 6;
 	buf->size = ft_strlen(buf->str);
@@ -37,15 +35,27 @@ void	convert_double(t_token *token, va_list ap, t_buf *buf)
 			exit(1);
 		(buf->size)++;
 	}
-	pow = 1;
+	value -= (long long)value;
+	if (value < 0)
+		value = -value;
+	pow = 1.0;
 	i = 0;
-	while (i < token->precision)
+	while (i < token->precision - 1)
 	{
-		pow *= 10;
+		pow *= 10.0;
 		if (!(buf->str = ft_strjoin_free(buf->str
-			, ft_lltoa((long long)((value - (long long)value) * pow) % 10))))
+			, ft_itoa((unsigned long long)(value * pow) % 10))))
 			exit(1);
 		(buf->size)++;
+		i++;
+	}
+	if (i < token->precision)
+	{
+		if (!(buf->str = ft_strjoin_free(buf->str
+			, ft_itoa(((unsigned long long)(value * pow) % 10)
+				+ (((unsigned long long)(value * pow * 10.0) % 10) >= 5)))))
+			exit(1);
+		(buf->size++);
 		i++;
 	}
 }
--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 15:32:56 by gguichar          #+#    #+#             */
-/*   Updated: 2018/11/20 00:08:46 by gguichar         ###   ########.fr       */
+/*   Updated: 2018/11/20 08:02:01 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 int	main(int argc, char **argv)
 {
-	float	test = -118.1284983505;
+	float	test = -118.6284983505;
 
 	(void)argc;
 	(void)argv;
-	printf("%.100f\n", test);
-	ft_printf("%.100f\n", test);
+	printf("%.7f\n", test);
+	ft_printf("%.7f\n", test);
 	return (0);
 }
