f530a230620b47df0707caf3fad0d74114497b29
Gwennael Guichard <gguichar@student.42.fr>
2018-12-13 09:45:30 +0100

    Ajout de la fonction ft_pow

--- filler/libft/includes/libft.h
+++ filler/libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/29 15:05:10 by gguichar          #+#    #+#             */
-/*   Updated: 2018/12/10 10:35:30 by gguichar         ###   ########.fr       */
+/*   Updated: 2018/12/13 09:23:03 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -205,6 +205,8 @@ int				ft_min(int a, int b);
 int				ft_max(int a, int b);
 
 int				ft_abs(int n);
+
+int				ft_pow(int n, int pow);
 
 size_t			ft_llsize(long long l);
 
