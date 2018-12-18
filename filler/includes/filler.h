322b90a86c22fcbb431648ab2cc076310b9cb9aa
Gwennael Guichard <gguichar@student.42.fr>
2018-12-18 13:53:48 +0100

    Norme

--- filler/includes/filler.h
+++ filler/includes/filler.h
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 10:51:12 by gguichar          #+#    #+#             */
-/*   Updated: 2018/12/13 17:09:55 by gguichar         ###   ########.fr       */
+/*   Updated: 2018/12/18 13:52:43 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,19 +41,19 @@ typedef struct	s_piece
 }				t_piece;
 
 /*
- ** PARSING part
- */
+** PARSING part
+*/
 void			*read_player(t_filler *filler);
 int				read_board(t_filler *filler);
 
-int			 	parse_piece(t_piece *piece);
+int				parse_piece(t_piece *piece);
 
 void			piece_fix_height(t_piece *piece);
 void			piece_fix_width(t_piece *piece);
 
 /*
- ** FILLER IQ
- */
+** FILLER IQ
+*/
 int				check_piece_pos(t_filler *filler, t_piece *piece
 		, int row, int col);
 void			iq_search_pos(t_filler *filler, t_piece *piece);
