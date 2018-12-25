fade5219e024b2d9315c1175ca598bc509ccc253
Gwennael Guichard <gguichar@student.42.fr>
2018-12-25 13:36:09 +0100

    Optimisation du quicksort

--- push_swap/src/quicksort.c
+++ push_swap/src/quicksort.c
@@ -6,48 +6,31 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 23:32:50 by gguichar          #+#    #+#             */
-/*   Updated: 2018/12/24 13:43:24 by gguichar         ###   ########.fr       */
+/*   Updated: 2018/12/25 13:32:25 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <limits.h>
 #include "libft.h"
 #include "rotations.h"
 #include "push_swap.h"
 
-static int	is_sorted(int n, int offset, t_list *lst)
+static int	is_sorted(int n, t_list *lst)
 {
-	int	index;
-
-	index = 0;
-	while (index < offset)
-	{
-		lst = lst->next;
-		index++;
-	}
-	index = 1;
-	while (index < n)
+	while (n > 1)
 	{
 		if (*((int *)lst->content) > *((int *)lst->next->content))
 			return (0);
 		lst = lst->next;
-		index++;
+		n--;
 	}
 	return (1);
 }
 
-static int	find_pivot(int n, t_list *lst)
-{
-	while (n > 1)
-	{
-		lst = lst->next;
-		n--;
-	}
-	return (*((int *)lst->content));
-}
-
 static int	is_lte_value(int n, t_list *lst, int value)
 {
-	while (n > 1)
+	while (n > 0)
 	{
 		if (*((int *)lst->content) > value)
 			return (0);
@@ -57,19 +40,65 @@ static int	is_lte_value(int n, t_list *lst, int value)
 	return (1);
 }
 
+static int	is_gt_value(int n, t_list *lst, int value)
+{
+	while (n > 0)
+	{
+		if (*((int *)lst->content) <= value)
+			return (0);
+		lst = lst->next;
+		n--;
+	}
+	return (1);
+}
+
+static int	find_pivot(int n, t_list *lst)
+{
+	int	tab[n];
+	int	i;
+	int	tmp;
+
+	i = 0;
+	while (i < n)
+	{
+		tab[i] = *((int *)lst->content);
+		lst = lst->next;
+		i++;
+	}
+	i = 1;
+	while (i < n)
+	{
+		if (tab[i - 1] > tab[i])
+		{
+			tmp = tab[i - 1];
+			tab[i - 1] = tab[i];
+			tab[i] = tmp;
+			if (i > 1)
+				i -= 1;
+			continue ;
+		}
+		i++;
+	}
+	tmp = tab[n / 2];
+	return (tmp);
+}
+
 static int	partition(int n, t_list **lst, t_list **tmp, int value)
 {
 	int	index;
 	int	pivot;
+	int	rots;
 
 	index = 0;
 	pivot = 0;
-	while (index < n)
+	rots = 0;
+	while (index < n && !is_gt_value(n - index, *lst, value))
 	{
-		if (*((int *)(*lst)->content) > value)
+		if (*((int *)(*lst)->content) >= value)
 		{
 			rotate(lst);
 			ft_putendl("ra");
+			rots++;
 		}
 		else
 		{
@@ -78,6 +107,18 @@ static int	partition(int n, t_list **lst, t_list **tmp, int value)
 			pivot++;
 		}
 		index++;
+	}
+	while (rots > 0)
+	{
+		rev_rotate(lst);
+		ft_putendl("rra");
+		if (*((int *)(*lst)->content) == value)
+		{
+			push(tmp, lst);
+			ft_putendl("pb");
+			pivot++;
+		}
+		rots--;
 	}
 	return (pivot);
 }
@@ -89,7 +130,7 @@ void		quicksort(int n, t_list **lst)
 	int				pivot;
 	int				index;
 
-	if (n < 2 || is_sorted(n, 0, *lst))
+	if (n < 2 || is_sorted(n, *lst))
 		return ;
 	if (n == 2)
 	{
@@ -103,13 +144,6 @@ void		quicksort(int n, t_list **lst)
 	else
 	{
 		pivot = partition(n, lst, &tmp, value);
-		index = pivot;
-		while (index < n)
-		{
-			rev_rotate(lst);
-			ft_putendl("rra");
-			index++;
-		}
 		quicksort(n - pivot, lst);
 		index = 0;
 		while (index < pivot)
