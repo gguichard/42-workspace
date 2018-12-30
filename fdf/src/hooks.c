2141c24618a59c6aa30adee8e870bf131127f75f
Gwennaël Guichard <gwennaelguich@gmail.com>
2018-12-30 06:14:58 +0100

    Ajout info en bas de la fenetre

--- fdf/src/hooks.c
+++ fdf/src/hooks.c
@@ -6,19 +6,31 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/30 04:05:38 by gguichar          #+#    #+#             */
-/*   Updated: 2018/12/30 05:32:13 by gguichar         ###   ########.fr       */
+/*   Updated: 2018/12/30 06:13:18 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
+#include <stdlib.h>
+#include "printf.h"
 #include "fdf.h"
 
 int	expose_hook(t_fdf *fdf)
 {
+	char	*str;
+
 	mlx_put_image_to_window(fdf->lib.mlx_ptr
 			, fdf->lib.win_ptr
 			, fdf->lib.img_ptr
 			, 0, 0);
+	ft_asprintf(&str, "Scale factor: %d | Depth factor: %d"
+			, fdf->scale, fdf->depth);
+	if (str != NULL)
+	{
+		mlx_string_put(fdf->lib.mlx_ptr
+				, fdf->lib.win_ptr, 50, fdf->height - 50, 0xFFFFFF, str);
+		free(str);
+	}
 	return (0);
 }
 
