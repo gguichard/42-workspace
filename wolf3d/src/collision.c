26af4b22e9349a1b8a91c72b0a7917f0b1ff4112
Gwennael Guichard <gguichar@student.42.fr>
2019-11-24 23:44:26 +0100

    wolf3d : amelioration des collisions

--- wolf3d/src/collision.c
+++ wolf3d/src/collision.c
@@ -6,7 +6,7 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/02 11:37:22 by gguichar          #+#    #+#             */
-/*   Updated: 2019/11/25 21:02:22 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/11/25 21:02:45 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,39 @@
 #include "tile_inf.h"
 #include "direction.h"
 
-static int	is_wall(int x, int y, t_map_inf *map_inf, t_direction dir)
+static int	is_wall_colliding(int x, int y, t_map_inf *map_inf, t_direction dir)
 {
 	t_tile_meta	*tile;
 
 	if (x < 0 || y < 0 || x >= map_inf->width || y >= map_inf->height)
 		return (1);
-	else
+	tile = &map_inf->tiles[y * map_inf->width + x];
+	return (tile->id != TILE_EMPTY
+		&& (tile->type != PORTAL_DATA || tile->data.portal.dir != dir));
+}
+
+static int	is_colliding(double x, double y, t_map_inf *map_inf
+	, t_direction dir)
+{
+	t_vec2i	start;
+	t_vec2i	end;
+	t_vec2i	pos;
+
+	start = vec2i(x - PLAYER_HALF_SIZE, y - PLAYER_HALF_SIZE);
+	end = vec2i(ceil(x + PLAYER_HALF_SIZE), ceil(y + PLAYER_HALF_SIZE));
+	pos.x = start.x;
+	while (pos.x < end.x)
 	{
-		tile = &map_inf->tiles[y * map_inf->width + x];
-		return (tile->id != TILE_EMPTY
-			&& (tile->type != PORTAL_DATA || tile->data.portal.dir != dir));
+		pos.y = start.y;
+		while (pos.y < end.y)
+		{
+			if (is_wall_colliding(pos.x, pos.y, map_inf, dir))
+				return (1);
+			pos.y += 1;
+		}
+		pos.x += 1;
 	}
+	return (0);
 }
 
 void		check_collision_after_move(t_ctx *ctx, t_vec2d old_pos)
@@ -57,16 +78,16 @@ void		check_collision_after_move(t_ctx *ctx, t_vec2d old_pos)
 		}
 	}
 	if (dir.x > 0
-		&& is_wall(pos.x + PLAYER_HALF_SIZE, old_pos.y, &ctx->tile_map, NORTH))
+		&& is_colliding(pos.x, old_pos.y, &ctx->tile_map, coll_dir))
 		pos.x = floor(pos.x + PLAYER_HALF_SIZE) - PLAYER_HALF_SIZE;
 	else if (dir.x < 0
-		&& is_wall(pos.x - PLAYER_HALF_SIZE, old_pos.y, &ctx->tile_map, SOUTH))
+		&& is_colliding(pos.x, old_pos.y, &ctx->tile_map, coll_dir))
 		pos.x = floor(pos.x - PLAYER_HALF_SIZE + 1) + PLAYER_HALF_SIZE;
 	if (dir.y > 0
-		&& is_wall(pos.x, pos.y + PLAYER_HALF_SIZE, &ctx->tile_map, WEST))
+		&& is_colliding(pos.x, pos.y, &ctx->tile_map, coll_dir))
 		pos.y = floor(pos.y + PLAYER_HALF_SIZE) - PLAYER_HALF_SIZE;
 	else if (dir.y < 0
-		&& is_wall(pos.x, pos.y - PLAYER_HALF_SIZE, &ctx->tile_map, EAST))
+		&& is_colliding(pos.x, pos.y, &ctx->tile_map, coll_dir))
 		pos.y = floor(pos.y - PLAYER_HALF_SIZE + 1) + PLAYER_HALF_SIZE;
 	ctx->player.position = pos;
 }
