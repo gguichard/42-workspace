eb9dc937e0594a9992e86a26d2d7564f6ffe0fe0
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-12-15 15:04:15 +0100

    wolf3d : suppression variable globale

--- wolf3d/src/wolf3d_events.c
+++ wolf3d/src/wolf3d_events.c
@@ -6,49 +6,55 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/30 14:22:59 by gguichar          #+#    #+#             */
-/*   Updated: 2019/12/04 09:22:34 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/12/15 15:02:34 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
 #include <SDL.h>
 #include "wolf3d.h"
 #include "keystates.h"
 
-static t_key	g_keys[] = {
-	{SDLK_ESCAPE, ESC_KEY},
-	{SDLK_RETURN, ENTER_KEY},
-	{SDLK_UP, MOVE_FORWARD_KEY},
-	{SDLK_DOWN, MOVE_BACKWARD_KEY},
-	{SDLK_w, MOVE_FORWARD_KEY},
-	{SDLK_s, MOVE_BACKWARD_KEY},
-	{SDLK_a, STRAFE_LEFT_KEY},
-	{SDLK_d, STRAFE_RIGHT_KEY},
-	{SDLK_LEFT, ROTATE_LEFT_KEY},
-	{SDLK_RIGHT, ROTATE_RIGHT_KEY}
-};
-
-static void	wolf3d_keys(t_ctx *ctx, SDL_Event *event)
+static uint64_t	wolf3d_key(int keycode)
 {
-	size_t	idx;
+	static t_key	keys[] = {
+		{SDLK_ESCAPE, ESC_KEY},
+		{SDLK_RETURN, ENTER_KEY},
+		{SDLK_UP, MOVE_FORWARD_KEY},
+		{SDLK_DOWN, MOVE_BACKWARD_KEY},
+		{SDLK_w, MOVE_FORWARD_KEY},
+		{SDLK_s, MOVE_BACKWARD_KEY},
+		{SDLK_a, STRAFE_LEFT_KEY},
+		{SDLK_d, STRAFE_RIGHT_KEY},
+		{SDLK_LEFT, ROTATE_LEFT_KEY},
+		{SDLK_RIGHT, ROTATE_RIGHT_KEY}
+	};
+	size_t			idx;
 
 	idx = 0;
-	while (idx < (sizeof(g_keys) / sizeof(g_keys[0])))
+	while (idx < (sizeof(keys) / sizeof(keys[0])))
 	{
-		if (g_keys[idx].keycode == event->key.keysym.sym)
-		{
-			if (event->type == SDL_KEYDOWN)
-				ctx->keystates |= g_keys[idx].bitmask;
-			else
-				ctx->keystates &= ~g_keys[idx].bitmask;
-			break ;
-		}
+		if (keys[idx].keycode == keycode)
+			return (keys[idx].bitmask);
 		idx++;
 	}
+	return (0);
 }
 
-void		wolf3d_events(t_ctx *ctx)
+static void		wolf3d_handle_keys(t_ctx *ctx, SDL_Event *event)
+{
+	uint64_t	bitmask;
+
+	bitmask = wolf3d_key(event->key.keysym.sym);
+	if (event->type == SDL_KEYDOWN)
+		ctx->keystates |= bitmask;
+	else
+		ctx->keystates &= ~bitmask;
+}
+
+void			wolf3d_events(t_ctx *ctx)
 {
 	SDL_Event	event;
 	int			ret;
@@ -63,6 +69,6 @@ void		wolf3d_events(t_ctx *ctx)
 		ret = ctx->states[ctx->state].evt_fn == NULL
 			|| !ctx->states[ctx->state].evt_fn(ctx, event);
 		if (ret && (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP))
-			wolf3d_keys(ctx, &event);
+			wolf3d_handle_keys(ctx, &event);
 	}
 }
