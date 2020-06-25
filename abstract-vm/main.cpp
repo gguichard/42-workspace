9fafb1f3f536f2277cb454cb96a306504dd178b4
Gwennael Guichard <gguichar@student.42.fr>
2020-06-25 09:40:59 +0200

    feat(abstract-vm): add instructions reading from stdin & files

--- abstract-vm/main.cpp
+++ abstract-vm/main.cpp
@@ -3,13 +3,47 @@
 #include "parser.hpp"
 #include "token.hpp"
 
-#include <queue>
+#include <cstdlib>
+#include <string>
 #include <iostream>
+#include <fstream>
+#include <queue>
+
+std::string readInput(std::istream &stream)
+{
+	std::string input;
+	std::string line;
+
+	while (std::getline(stream, line) && line != ";;") {
+		input += line;
+		if (stream.good()) {
+			input += "\n";
+		}
+	}
+	return input;
+}
 
 int main(int argc, char **argv)
 {
 	AbstractVM vm;
-	Lexer lexer(argv[1]);
+	std::string input;
+	std::string line;
+
+	if (argc > 2) {
+		std::cerr << "usage: " << argv[0] << " [file]" << std::endl;
+		return EXIT_FAILURE;
+	} else if (argc < 2) {
+		input = readInput(std::cin);
+	} else {
+		std::ifstream file(argv[1]);
+
+		if (!file.is_open()) {
+			std::cerr << "Unable to open file " << argv[1] << std::endl;
+			return EXIT_FAILURE;
+		}
+		input = readInput(file);
+	}
+	Lexer lexer(input);
 	Parser parser(lexer);
 
 	try {
@@ -23,5 +57,5 @@ int main(int argc, char **argv)
 	} catch (VMException &e) {
 		std::cerr << "VM error: " << e.what() << std::endl;
 	}
-	return 0;
+	return EXIT_SUCCESS;
 }
